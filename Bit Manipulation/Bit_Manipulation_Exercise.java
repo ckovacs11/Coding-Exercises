e38bc95b4aea682cb4129c342b4ea72946869aa9
Curtis Kovacs <ckovacs1@asu.edu>
2021-06-05 22:21:30 -0600

    Add bitshift method and update Readme

--- Bit Manipulation/Bit_Manipulation_Exercise.java
+++ Bit Manipulation/Bit_Manipulation_Exercise.java
@@ -14,6 +14,16 @@ public class Bit_Manipulation_Exercise {
         int result = ~x;
         return result;
     }
+    //if bits in corresponding positions are different
+    private int bitwiseXor(int x, int y) {
+        int result = x ^ y;
+        return result;
+    }
+    //bit shift left 1
+    private int bitwiseLeftShiftOne(int x) {
+        int result = x << 1;
+        return result;
+    }
     public static void main(String[] args) {
 
         Bit_Manipulation_Exercise bme = new Bit_Manipulation_Exercise();
@@ -21,6 +31,8 @@ public class Bit_Manipulation_Exercise {
         System.out.println(bme.bitwiseAnd(4, 5)); //100 & 101 = 100, or 4
         int complementResult = bme.bitwiseComplement(2);
         System.out.println(complementResult + " and the binary representation: " + Integer.toBinaryString(complementResult));//11111111111111111111111111111101
+        System.out.println(bme.bitwiseXor(4, 5)); // 100 ^ 101 = 001
+        System.out.println(bme.bitwiseLeftShiftOne(5)); //101 << 1 = 1010, or 10
         
 
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Coding-Exercises
+
+Various Java coding concepts are explored. All .java files need to be compiled and run in a JVM (preferrably with a JDK).
